6747445afaf4b89db9c31eb2ff13712ec1a39f3e
dino-ha <hadzirovicdino@gmail.com>
2022-03-31 20:05:14 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,13 @@
+function main(numOne, numTwo){
 
+	var product = 0.0;
+	numOne = 0.0;
+	numTwo = 0.0;
+	product = numOne * numTwo;
+	return product;
+
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
