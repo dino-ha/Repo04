fb1c79f58e4c804ad99d0717a6c034dbf2333ae7
dino-ha <hadzirovicdino@gmail.com>
2022-03-31 20:02:39 +0200

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,16 @@
+function main(numOne, numTwo){
 
+	var quotient;
+	if(numTwo === 0){
+		quotient = 'Divide by 0! Cannot divide by zero!';
+	} else{
+
+		quotient = numOne / numTwo;
+	}
+	
+	return quotient;
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
