f77f143221bfa2a936b3769e16474e3ddec796cb
dino-ha <hadzirovicdino@gmail.com>
2022-03-31 20:16:36 +0200

    Update index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,13 +1,13 @@
 function main(numOne, numTwo){
 
 	var myQuotient = 0.0;
-	numOne = 0.0;
-	numTwo = 0.0;
+	var numOne = 0.0;
+	var numTwo = 0.0;
 	myQuotient = numOne / numTwo;
 	
 
 }
 
-main(numOne, numTwo);
+console.log(main(numOne, numTwo));
 
 module.exports = main;
