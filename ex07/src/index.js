803c19e31593bb9959ef40fd7f525af5fdcd49c8
dino-ha <hadzirovicdino@gmail.com>
2022-03-31 20:06:21 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,15 @@
+function main(numOne, numTwo){
 
+	var remainder;
+	numOne = 0;
+	numTwo = 0;
+	remainder = numOne % numTwo;
+	return remainder;
+
+	
+
+}
+
+main(numOne, numTwo);
+
+module.exports = main;
