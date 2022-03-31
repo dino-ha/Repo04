77c5f64b7aa69547643212bbeaf61399b03cc1ea
dino-ha <hadzirovicdino@gmail.com>
2022-03-31 20:07:29 +0200

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,22 @@
+function main(){
 
+	myX = 12;
+	myY = 19;
+	myZ = 24;
+	myX += 3;
+	myY += 17;
+	myZ -= 20;
+	
+	return {
+
+		myX,
+		myY,
+		myZ
+	};
+	
+
+}
+
+console.log(main());
+
+module.exports = main;
